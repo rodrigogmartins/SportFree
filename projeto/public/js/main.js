0696f9d9a45e30d40e4ccf9e376a7a34379244b2
Rodrigo Martins <contawarface750@gmail.com>
2018-04-28 15:45:33 -0300

    Add files via upload

--- projeto/public/js/main.js
+++ projeto/public/js/main.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const audio = document.querySelector('audio');
     const input = document.querySelector('#loader');
-    const player = document.querySelector('#player');
-    const ol = document.querySelector('ol');
+    const player = document.querySelector('#audioplayer');
+    const ul = document.querySelector('ul');
     const proxima = document.querySelector('#proxima');
     const voltar = document.querySelector('#voltar');
-    let playlist = [];
     let indiceMusica = 0;
-    let nomesMusicas = [];
+    const nomesMusicas = [];
+    const playlist = [];
 
-    input.addEventListener('change', function(){
+    input.addEventListener('change', function() {
         const reader = new FileReader();
         reader.addEventListener('load', function() {
-            playlist.push(reader.result);
-            addlist();
-            tocarMusica(indiceMusica);
+            const musica = reader.result;
+            if (playlist.indexOf(musica) === -1) {
+                playlist.push(musica);
+                addlist();
+            }
         });
         reader.readAsDataURL(input.files[0]);
     });
 
-    ol.addEventListener('click', function(e){
-        console.log(e.target.tagName);
+    ul.addEventListener('click', function(e) {
         if (e.target.tagName === 'A') {
             indiceMusica = nomesMusicas.indexOf(e.target.textContent);
             tocarMusica(indiceMusica);
@@ -33,14 +34,15 @@ document.addEventListener('DOMContentLoaded', function() {
             indiceMusica = 0;
         }
         tocarMusica(indiceMusica);
-    }
+    };
+
     const anteriorMusica = function() {
         indiceMusica--;
         if (indiceMusica < 0) {
             indiceMusica = playlist.length-1;
         }
         tocarMusica(indiceMusica);
-    }
+    };
 
     audio.addEventListener('ended', proximaMusica);
     proxima.addEventListener('click', proximaMusica);
@@ -50,23 +52,23 @@ document.addEventListener('DOMContentLoaded', function() {
         player.src = playlist[indice];
         player.play();
         const labelMusica = document.querySelector('#nomemusica');
-        labelMusica.textContent = ol.children[indice].textContent;
-    }
+        labelMusica.textContent = ul.children[indice].textContent;
+    };
 
     const addlist = function() {
-        let nomeMusica = loader.files[0].name;
-        let nomeArrumado = arrumarNomeMusica(nomeMusica);
+        const nomeMusica = input.files[0].name;
+        const nomeArrumado = arrumarNomeMusica(nomeMusica);
         nomesMusicas.push(nomeArrumado);
         const li = document.createElement('li');
         const a = document.createElement('a');
-        a.setAttribute('href','#');
-        a.textContent = nomeArrumado
+        a.setAttribute('href', '#');
+        a.textContent = nomeArrumado;
         li.appendChild(a);
-        ol.appendChild(li);
-    }
+        ul.appendChild(li);
+    };
 
     const arrumarNomeMusica = function(nome) {
-        let nomeVetor = nome.split('.');
+        const nomeVetor = nome.split('.');
         return nomeVetor[0];
-    }
+    };
 });
